562fd199d13790816d3c825a00439a452b9a934a
yukichi0324 <kajitani.code@gmail.com>
2024-08-02 17:11:26 +0900

    整形

--- src/components/PuzzleGrid.vue
+++ src/components/PuzzleGrid.vue
@@ -1,37 +1,36 @@
 <!-- src/components/PuzzleGrid.vue -->
 <template>
     <div class="grid-container">
-      <div class="grid">
-        <div v-for="n in 48" :key="n" class="grid-cell"></div>
-      </div>
+        <div class="grid">
+            <div v-for="n in 48" :key="n" class="grid-cell"></div>
+        </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
+</template>
+
+<script>
+export default {
     name: 'PuzzleGrid'
-  }
-  </script>
-  
-  <style scoped>
-  .grid-container {
+}
+</script>
+
+<style scoped>
+.grid-container {
     display: flex;
     justify-content: center;
     align-items: center;
-  }
-  
-  .grid {
+}
+
+.grid {
     display: grid;
     grid-template-columns: repeat(6, 50px);
     grid-template-rows: repeat(8, 50px);
     gap: 5px;
-  }
-  
-  .grid-cell {
+}
+
+.grid-cell {
     width: 50px;
     height: 50px;
     background-color: lightgray;
     border: 1px solid lightgray;
-  }
-  </style>
-  
+}
+</style>
