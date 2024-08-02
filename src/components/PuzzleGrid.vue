040dd7e8719e0212da186e9ba3fc2519a5ef27e3
yukichi0324 <kajitani.code@gmail.com>
2024-08-02 18:28:54 +0900

    衝突判定、ランダムな数字を落とす

--- src/components/PuzzleGrid.vue
+++ src/components/PuzzleGrid.vue
@@ -2,8 +2,11 @@
     <div class="grid-container">
         <div class="grid">
             <div v-for="n in 48" :key="n" class="grid-cell"></div>
-            <div v-for="cell in cells" :key="cell.x + '-' + cell.y" :style="cellStyle(cell)">
+            <div v-for="cell in staticCells" :key="cell.x + '-' + cell.y" :style="cellStyle(cell)">
                 {{ cell.value }}
+            </div>
+            <div v-if="fallingCell" :style="cellStyle(fallingCell)">
+                {{ fallingCell.value }}
             </div>
         </div>
     </div>
@@ -14,7 +17,10 @@ export default {
     name: 'PuzzleGrid',
     data() {
         return {
-            cells: [{ value: 1, x: 2, y: 3 }]
+            staticCells: [],
+            fallingCell: null,
+            nextCell: { value: 9, x: 3, y: 0 }, // 初期の落ちるセルの設定
+            intervalId: null
         }
     },
     methods: {
@@ -30,22 +36,46 @@ export default {
                 zIndex: 1
             };
         },
-        moveCellsDown() {
-            console.log('call moveCellsDown')
-            //TODO:forEachが嫌だ
-            this.cells.forEach(cell => {
-                if (cell.y < 7) {
-                    cell.y += 1;
-                }
-            });
+        isCollision(newCell) {
+            // セルがグリッド外に出ているか、他のセルと衝突するかチェック
+            return (
+                newCell.x < 0 ||
+                newCell.x >= 6 ||
+                newCell.y >= 8 ||
+                this.staticCells.some(cell => cell.x === newCell.x && cell.y === newCell.y)
+            );
         },
+        moveCellsDown() {
+            console.log('called move');
+            if (this.fallingCell) {
+                // 現在落ちているセルがある場合
+                const newCell = { ...this.fallingCell, y: this.fallingCell.y + 1 };
+                console.log('newCell',newCell);
+
+                if (this.isCollision(newCell)) {
+                    // 一番下に到達したか、他のセルと衝突した場合
+                    this.staticCells.push(this.fallingCell);
+                    this.fallingCell = null; // 落ちているセルがなくなった
+                } else {
+                    this.fallingCell.y += 1; // セルを1つ下に移動
+                }
+            } else {
+                console.log('new cell');
+                console.log('this.fallingCell-1',this.fallingCell);
+                // 落ちているセルがない場合、新しいセルを生成
+                this.fallingCell = { ...this.nextCell, y: 0 };
+                console.log('this.fallingCell-2',this.fallingCell);
+
+                console.log('this.nextCell',this.nextCell)
+                this.nextCell = { value: Math.floor(Math.random() * 10), x: Math.floor(Math.random() * 6), y: 0 };
+                console.log('this.nextCell-2',this.nextCell)
+            }
+        }
     },
     created() {
-        console.log('created')
         this.intervalId = setInterval(this.moveCellsDown, 1000);
     },
     beforeUnmount() {
-        console.log('beforeDestroy')
         clearInterval(this.intervalId);
     }
 }
